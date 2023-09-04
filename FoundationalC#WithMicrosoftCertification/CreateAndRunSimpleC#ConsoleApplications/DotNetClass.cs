1428330f444c9229dd7b3432b6024b0664893aad
isaiisj <47548422+isaiisj@users.noreply.github.com>
2023-09-04 15:23:26 -0600

    Create DotNetClass.cs

---
+++ FoundationalC#WithMicrosoftCertification/CreateAndRunSimpleC#ConsoleApplications/DotNetClass.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int firstValue = 500;
+    int secondValue = 600;
+    int largerValue;
+    largerValue = Math.Max(firstValue,secondValue);
+    Console.WriteLine(largerValue);
+  }
+}
