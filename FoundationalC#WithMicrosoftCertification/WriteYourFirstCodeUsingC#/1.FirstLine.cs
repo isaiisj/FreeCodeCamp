7ed6896ff2ae6785b5adeecabf3cccb418b4c6d8
Isai <47548422+isaiisj@users.noreply.github.com>
2023-11-01 00:35:58 -0600

    Create 1.FirstLine.cs

---
+++ FoundationalC#WithMicrosoftCertification/WriteYourFirstCodeUsingC#/1.FirstLine.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.Write("Congratulations!");
+    Console.Write(" ");
+    Console.Write("You wrote your first lines of code.");
+    Console.WriteLine(" ");
+    Console.WriteLine("Another Line");
+  }
+}
