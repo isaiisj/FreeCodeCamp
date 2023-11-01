a3faa5e42c9ad7c8e805d3113f2e6abc936b411e
Isai <47548422+isaiisj@users.noreply.github.com>
2023-11-01 02:10:37 -0600

    Create 3.1.ConditionalOperators.cs

---
+++ FoundationalC#WithMicrosoftCertification/AddLogicToC#ConsoleAplications/3.1.ConditionalOperators.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Random coin = new Random();
+    int flip = coin.Next(0, 2);
+    Console.WriteLine((flip == 0) ? "heads" : "tails");
+  }
+}
