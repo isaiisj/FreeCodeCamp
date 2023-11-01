22484afc4d1c75473376e3fa15b35911e220df79
Isai <47548422+isaiisj@users.noreply.github.com>
2023-11-01 00:51:16 -0600

    Update FarenheitToCelsius.cs

--- FoundationalC#WithMicrosoftCertification/WriteYourFirstCodeUsingC#/FarenheitToCelsius.cs
+++ FoundationalC#WithMicrosoftCertification/WriteYourFirstCodeUsingC#/FarenheitToCelsius.cs
@@ -6,5 +6,9 @@ class Program {
     float FtoC = (farenheit-32F)*(5F/9F);
 
     Console.WriteLine($"The temperature is {FtoC} celsius.");
+
+    //Otra posible solucion
+    decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+    Console.WriteLine("The temperature is " + celsius + " Celsius.");
   }
 }
