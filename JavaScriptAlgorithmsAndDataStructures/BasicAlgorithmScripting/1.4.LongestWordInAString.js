29840116c463707ee29cc67cb40a7a9ad5e0ec7c
Isai <47548422+isaiisj@users.noreply.github.com>
2023-11-12 19:21:18 -0600

    Create 1.4.LongestWordInAString.js

---
+++ JavaScriptAlgorithmsAndDataStructures/BasicAlgorithmScripting/1.4.LongestWordInAString.js
@@ -0,0 +1,14 @@
+function findLongestWordLength(str) {
+  let strArr = str.split(" ");
+  let length = 0;
+  for(let i in strArr){
+     let x = strArr[i].length;
+     if(length < x ){
+       length = x;
+     }
+  }
+  //console.log(length);
+  return length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
