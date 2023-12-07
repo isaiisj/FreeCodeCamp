cdbe0c42996fa644b685278b920ed912e02aa25e
Isai <47548422+isaiisj@users.noreply.github.com>
2023-12-07 02:12:46 -0600

    Update 4.LongestWordInAString.js

--- JavaScriptAlgorithmsAndDataStructures/BasicAlgorithmScripting/4.LongestWordInAString.js
+++ JavaScriptAlgorithmsAndDataStructures/BasicAlgorithmScripting/4.LongestWordInAString.js
@@ -1,3 +1,9 @@
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+*/
+
 function findLongestWordLength(str) {
   let strArr = str.split(" ");
   let length = 0;
